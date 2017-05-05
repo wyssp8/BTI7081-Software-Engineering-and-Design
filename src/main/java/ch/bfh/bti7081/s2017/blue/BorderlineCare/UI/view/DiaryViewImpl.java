a79eff77400600e9df2dee23bc0d4ee86c9040d4
Martin Kieliger <martin-k@hotmail.ch>
2017-05-05 13:44:40 +0200

    Modify DiaryView

--- src/main/java/ch/bfh/bti7081/s2017/blue/BorderlineCare/UI/view/DiaryViewImpl.java
+++ src/main/java/ch/bfh/bti7081/s2017/blue/BorderlineCare/UI/view/DiaryViewImpl.java
@@ -1,37 +1,45 @@
 package ch.bfh.bti7081.s2017.blue.BorderlineCare.UI.view;
 
 import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.List;
 
 import com.vaadin.ui.Button;
 import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.DateField;
 import com.vaadin.ui.Grid;
 import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.TextField;
+import com.vaadin.ui.TextArea;
 import com.vaadin.ui.VerticalLayout;
 
 public class DiaryViewImpl extends CustomComponent {
 	
 	
 	public DiaryViewImpl(){
-		VerticalLayout vLayout = new VerticalLayout();
-		HorizontalLayout hLayout = new HorizontalLayout();
 		
-		setCompositionRoot(hLayout);
+		VerticalLayout vLayout = new VerticalLayout();
+		//HorizontalLayout hLayout = new HorizontalLayout();
+		
+		//setCompositionRoot(hLayout);
+		setCompositionRoot(vLayout);
+		
 		
 		// Create a DateField
-		DateField date = new DateField("Date");
-		hLayout.addComponent(date);
+		DateField date = new DateField();
+		vLayout.addComponent(date);
 		date.setValue(LocalDate.now());
 		
-		// Create a text field
-		TextField tf = new TextField("A Field");
-		hLayout.addComponent(tf);
-		tf.setValue("Some Text");
+		// Create a text area
+		TextArea txtArea = new TextArea("Diary entry");
+		txtArea.setValue("What have you done today\n" + "How did you feel today?");
+		vLayout.addComponent(txtArea);
 		
-		// Create Grid table
+		//Button
+		Button button = new Button("Add");
+		vLayout.addComponent(button);
+		
+		//Grid Table
+		Grid grid = new Grid("My diary entries");
+		vLayout.addComponent(grid);
+		
 		
 	}
 
