9aaadfcbcf8b21208f0ecae3197256981b8a5247
Joël Frutiger <jooolchef@gmail.com>
2017-05-15 19:15:18 +0200

    remove unused imports

--- src/main/java/ch/bfh/bti7081/s2017/blue/BorderlineCare/UI/presenter/EmergencyViewPresenter.java
+++ src/main/java/ch/bfh/bti7081/s2017/blue/BorderlineCare/UI/presenter/EmergencyViewPresenter.java
@@ -1,11 +1,7 @@
 package ch.bfh.bti7081.s2017.blue.BorderlineCare.UI.presenter;
 
-import com.vaadin.icons.VaadinIcons;
 import com.vaadin.server.Page;
-import com.vaadin.server.Resource;
-
 import ch.bfh.bti7081.s2017.blue.BorderlineCare.UI.model.EmergencyViewModel;
-import ch.bfh.bti7081.s2017.blue.BorderlineCare.UI.presenter.interfaces.ButtonClickListener;
 import ch.bfh.bti7081.s2017.blue.BorderlineCare.UI.presenter.interfaces.EmergencyButtonClickListener;
 import ch.bfh.bti7081.s2017.blue.BorderlineCare.UI.view.mainView.EmergencyViewImpl;
 /**
