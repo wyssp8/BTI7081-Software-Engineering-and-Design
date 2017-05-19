da6a70a0153e5be43918d38c0f1199a73b561d63
Joël Frutiger <jooolchef@gmail.com>
2017-05-19 14:21:25 +0200

    exercise state, plus presentation

--- src/main/java/ch/bfh/bti7081/s2017/blue/BorderlineCare/UI/model/ExercisesViewModel.java
+++ src/main/java/ch/bfh/bti7081/s2017/blue/BorderlineCare/UI/model/ExercisesViewModel.java
@@ -2,8 +2,13 @@ package ch.bfh.bti7081.s2017.blue.BorderlineCare.UI.model;
 
 import java.util.ArrayList;
 
+import ch.bfh.bti7081.s2017.blue.BorderlineCare.UI.model.exercise.CalmDownExercise;
 import ch.bfh.bti7081.s2017.blue.BorderlineCare.UI.model.exercise.DailyExercise;
 import ch.bfh.bti7081.s2017.blue.BorderlineCare.UI.model.exercise.Exercise;
+import ch.bfh.bti7081.s2017.blue.BorderlineCare.UI.model.exercise.state.ExcerciseDone;
+import ch.bfh.bti7081.s2017.blue.BorderlineCare.UI.model.exercise.state.ExercisePending;
+import ch.bfh.bti7081.s2017.blue.BorderlineCare.UI.model.exercise.state.State;
+
 /**
  * 
  * @author wyssp8
@@ -11,22 +16,34 @@ import ch.bfh.bti7081.s2017.blue.BorderlineCare.UI.model.exercise.Exercise;
  */
 public class ExercisesViewModel {
 
-	private ArrayList<Exercise> exercises = new ArrayList<Exercise>();
-	
-	public ExercisesViewModel(){
-		exercises.add(new DailyExercise("Puzzle", "this is what you have to do", "puzzle.png"));
-		exercises.add(new DailyExercise("Read a book", "this is what you have to do", "reading.png"));
-		exercises.add(new DailyExercise("Walk", "this is what you have to do", "walking.png"));
-		exercises.add(new DailyExercise("Write a letter", "Please open the door", "letter.png"));
-		exercises.add(new DailyExercise("Write a diary", "Please open the door", "diary.png"));
-		exercises.add(new DailyExercise("Clean you room", "Please open the door", "cleaning.png"));
+	private ArrayList<Exercise> calmDownExercises = new ArrayList<Exercise>();
+	private ArrayList<Exercise> dailyExercises = new ArrayList<Exercise>();
+
+	public ExercisesViewModel() {
+		calmDownExercises.add(new CalmDownExercise("Puzzle", "this is what you have to do", "puzzle.png"));
+		calmDownExercises.add(new CalmDownExercise("Read a book", "this is what you have to do", "reading.png"));
+		calmDownExercises.add(new CalmDownExercise("Walk", "this is what you have to do", "walking.png"));
+		calmDownExercises.add(new CalmDownExercise("Write a letter", "Please open the door", "letter.png"));
+		calmDownExercises.add(new CalmDownExercise("Write a diary", "Please open the door", "diary.png"));
+		calmDownExercises.add(new CalmDownExercise("Clean you room", "Please open the door", "cleaning.png"));
+		
+		dailyExercises.add(new DailyExercise("Take a Walk", "Go outside and take a walk", "walking.png"));
+		dailyExercises.add(new DailyExercise("Diary entry", "Write your daily diary entry.", "diary.png"));
+		dailyExercises.add(new DailyExercise("Reading", "Read 10 pages of your book.", "reading.png"));
+		
+		
+	}
+
+	public void addExercise(Exercise exercise) {
+		calmDownExercises.add(exercise);
+	}
+
+	public ArrayList<Exercise> getExercisesCalm() {
+		return calmDownExercises;
 	}
 	
-	public void addExercise(Exercise exercise){
-		exercises.add(exercise);
+	public ArrayList<Exercise> getExercisesDaily() {
+		return dailyExercises;
 	}
-	
-	public ArrayList<Exercise> getExercises() {
-		return exercises;
-	}
+
 }
--- src/main/java/ch/bfh/bti7081/s2017/blue/BorderlineCare/UI/model/exercise/DailyExercise.java
+++ src/main/java/ch/bfh/bti7081/s2017/blue/BorderlineCare/UI/model/exercise/DailyExercise.java
@@ -1,4 +1,10 @@
 package ch.bfh.bti7081.s2017.blue.BorderlineCare.UI.model.exercise;
+
+import java.util.List;
+
+import ch.bfh.bti7081.s2017.blue.BorderlineCare.UI.model.exercise.state.ExercisePending;
+import ch.bfh.bti7081.s2017.blue.BorderlineCare.UI.model.exercise.state.State;
+
 /**
  * 
  * @author wyssp8
@@ -6,8 +12,19 @@ package ch.bfh.bti7081.s2017.blue.BorderlineCare.UI.model.exercise;
  */
 public class DailyExercise extends Exercise {
 
+	private State state;
+	
 	public DailyExercise(String title, String description, String path) {
 		super(title, description, path);
+		setState(new ExercisePending());
+	}
+
+	public void setState(State state) {
+		this.state = state;
+	}
+
+	public State getState() {
+		return state;
 	}
 
 }
