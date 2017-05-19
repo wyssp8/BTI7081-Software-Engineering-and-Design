289828e9492f483cb24aef0ea8eed8a405e89744
ludes2 <sandro.luder@students.bfh.ch>
2017-05-19 08:20:58 +0200

    added new functions (add and delete a contact)

--- src/main/java/ch/bfh/bti7081/s2017/blue/BorderlineCare/UI/presenter/ContactButtonClickListener.java
+++ src/main/java/ch/bfh/bti7081/s2017/blue/BorderlineCare/UI/presenter/ContactButtonClickListener.java
@@ -1,7 +1,13 @@
 package ch.bfh.bti7081.s2017.blue.BorderlineCare.UI.presenter;
 
+import com.vaadin.ui.PopupView;
+
+import ch.bfh.bti7081.s2017.blue.BorderlineCare.UI.model.Contact;
+
 public interface ContactButtonClickListener {
-	public void deleteButtonClick();
-	public void newContactButtonClick();
+	public Contact deleteButtonClick(PopupView popuu, Contact toDelete);
+	public void saveButtonClick(String stringInput, int integerInput);
+	public void cancelButtonClick();
+	public void deleteContact();
 
 }
