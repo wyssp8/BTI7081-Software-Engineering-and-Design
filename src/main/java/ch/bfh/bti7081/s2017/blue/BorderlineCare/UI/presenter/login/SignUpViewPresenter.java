fc0f15703aad91c50320ef031cb2cff0ea1ad929
poloc1 <poloc1@bfh.ch>
2017-06-13 21:05:01 +0200

    added Util test

--- src/main/java/ch/bfh/bti7081/s2017/blue/BorderlineCare/UI/presenter/login/SignUpViewPresenter.java
+++ src/main/java/ch/bfh/bti7081/s2017/blue/BorderlineCare/UI/presenter/login/SignUpViewPresenter.java
@@ -80,41 +80,5 @@ public class SignUpViewPresenter extends CustomComponent implements SignUpViewBu
 	@Override
 	public void cancelButtonClick() {
 		navigator.navigateTo("LoginView");	
-	}
-//	
-//	
-//	//Generate secure password with PBKDF2 encryption
-//	private static String generateStorngPasswordHash(String password) throws NoSuchAlgorithmException, InvalidKeySpecException
-//    {
-//        int iterations = 1000;
-//        char[] chars = password.toCharArray();
-//        byte[] salt = getSalt();
-//         
-//        PBEKeySpec spec = new PBEKeySpec(chars, salt, iterations, 64 * 8);
-//        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-//        byte[] hash = skf.generateSecret(spec).getEncoded();
-//        return iterations + ":" + toHex(salt) + ":" + toHex(hash);
-//    }
-//     
-//    private static byte[] getSalt() throws NoSuchAlgorithmException
-//    {
-//        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
-//        byte[] salt = new byte[16];
-//        sr.nextBytes(salt);
-//        return salt;
-//    }
-//     
-//    private static String toHex(byte[] array) throws NoSuchAlgorithmException
-//    {
-//        BigInteger bi = new BigInteger(1, array);
-//        String hex = bi.toString(16);
-//        int paddingLength = (array.length * 2) - hex.length();
-//        if(paddingLength > 0)
-//        {
-//            return String.format("%0"  +paddingLength + "d", 0) + hex;
-//        }else{
-//            return hex;
-//        }
-//    }
-    
+	}    
 }
