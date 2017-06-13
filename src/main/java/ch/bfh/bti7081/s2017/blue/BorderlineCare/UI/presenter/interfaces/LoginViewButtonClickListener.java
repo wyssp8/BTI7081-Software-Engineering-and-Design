b4b6c827940e67767bed166d14a1d64913754fb4
poloc1 <poloc1@bfh.ch>
2017-06-13 21:20:46 +0200

    removed bypass login

--- src/main/java/ch/bfh/bti7081/s2017/blue/BorderlineCare/UI/presenter/interfaces/LoginViewButtonClickListener.java
+++ src/main/java/ch/bfh/bti7081/s2017/blue/BorderlineCare/UI/presenter/interfaces/LoginViewButtonClickListener.java
@@ -9,6 +9,5 @@ package ch.bfh.bti7081.s2017.blue.BorderlineCare.UI.presenter.interfaces;
 public interface LoginViewButtonClickListener {
 	public void loginButtonClick();
 	public void signUpButtonClick();
-	public void bypassButtonClick();
 	public boolean validateLogin();
 }
