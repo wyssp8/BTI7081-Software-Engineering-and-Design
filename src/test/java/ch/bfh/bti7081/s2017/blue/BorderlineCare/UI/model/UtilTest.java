82ce02f43b2b49cd52a2e017f4d9e4f1d5967e64
ludes2 <sandro.luder@students.bfh.ch>
2017-06-15 20:15:10 +0200

    implemented UnitTest for Contact

--- src/test/java/ch/bfh/bti7081/s2017/blue/BorderlineCare/UI/model/UtilTest.java
+++ src/test/java/ch/bfh/bti7081/s2017/blue/BorderlineCare/UI/model/UtilTest.java
@@ -1,46 +1,46 @@
-//package ch.bfh.bti7081.s2017.blue.BorderlineCare.UI.model;
-//
-//import static org.junit.Assert.*;
-//
-//import java.security.NoSuchAlgorithmException;
-//import java.security.spec.InvalidKeySpecException;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import ch.bfh.bti7081.s2017.blue.BorderlineCare.Util;
-//import ch.bfh.bti7081.s2017.blue.BorderlineCare.DB.DBConnector;
-//
-//public class UtilTest {
-//	
-//	Util util = new Util();
-//	String generatedSecuredPasswordHash = null;
-//	boolean passwordMatched;
-//	String currentPassword;
-//	@Before
-//	public void setUp(){
-//		 currentPassword = "test";
-//	}
-//	
-//	@Test
-//	public void testPasswordHash() {
-//
-//		
-//		try {
-//			generatedSecuredPasswordHash = util.generateStorngPasswordHash(currentPassword);
-//		} catch (NoSuchAlgorithmException e) {
-//			e.printStackTrace();
-//		} catch (InvalidKeySpecException e) {
-//			e.printStackTrace();
-//		}
-//		try {
-//			passwordMatched = util.validatePassword(currentPassword, generatedSecuredPasswordHash);
-//		} catch (NoSuchAlgorithmException e) {
-//			e.printStackTrace();
-//		} catch (InvalidKeySpecException e) {
-//			e.printStackTrace();
-//		}
-//		assertTrue(passwordMatched);
-//	}
-//
-//}
+package ch.bfh.bti7081.s2017.blue.BorderlineCare.UI.model;
+
+import static org.junit.Assert.*;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ch.bfh.bti7081.s2017.blue.BorderlineCare.Util;
+import ch.bfh.bti7081.s2017.blue.BorderlineCare.DB.DBConnector;
+
+public class UtilTest {
+	
+	Util util = new Util();
+	String generatedSecuredPasswordHash = null;
+	boolean passwordMatched;
+	String currentPassword;
+	@Before
+	public void setUp(){
+		 currentPassword = "test";
+	}
+	
+	@Test
+	public void testPasswordHash() {
+
+		
+		try {
+			generatedSecuredPasswordHash = util.generateStorngPasswordHash(currentPassword);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (InvalidKeySpecException e) {
+			e.printStackTrace();
+		}
+		try {
+			passwordMatched = util.validatePassword(currentPassword, generatedSecuredPasswordHash);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (InvalidKeySpecException e) {
+			e.printStackTrace();
+		}
+		assertTrue(passwordMatched);
+	}
+
+}
